3ab902f9a587a26a6e0b1feaa2ea466a4d57b3f0
Dafydd Carver-Williams <d.carverwilliams@gmail.com>
2025-12-01 12:40:31 +0000

    Completed Day 1 Part 1

--- AoC2025-1.py
+++ AoC2025-1.py
@@ -1,1 +1,52 @@
+# L = -, R = +. get new number then +100 if <0 or -100 if >99?
+test_input = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+puzzle_input = str(open("Day1Input.txt").read())
+default_pos = 50
+current_pos = default_pos
+password = 0
 
+def ingester(string):
+    
+    instructions = []
+    instructions = string.split("\n")
+    for i in range(len(instructions)-1):
+        print(i, instructions[i])
+        instructions[i] = [instructions[i][0],int(instructions[i][1:])]
+    #we now have an array of instructions in the format ["L", 99]
+    return instructions
+    
+def dialer(current_pos, instruction):
+    
+    if instruction[0] == "L":           # simulate a turn to the left or right
+        current_pos -= instruction[1]
+    elif instruction[0] == "R":
+        current_pos += instruction[1]
+    else:
+        print("bruh?")
+        
+    while current_pos < 0:                 # correct for looping set of numbers
+        current_pos += 100
+    while current_pos > 99:
+        current_pos -= 100
+    return current_pos
+
+#-------------------------------------------------
+#instructions = ingester(test_input)
+instructions = ingester(puzzle_input)
+
+for i in range(len(instructions)-1):
+    current_pos = dialer(current_pos, instructions[i])
+    print(current_pos)
+    if current_pos == 0:
+        password += 1
+        
+print("\nPassword: ", password)
