5c1a3f4b41ce980ff0beaae1e14aacbc11f43fb0
Dafydd Carver-Williams <d.carverwilliams@gmail.com>
2025-12-02 20:54:06 +0000

    reverted back to part 1 solution for archival

--- AoC2025-01.py
+++ AoC2025-01.py
@@ -1,31 +1,22 @@
 # L = -, R = +. get new number then +100 if <0 or -100 if >99?
-test_input = """L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82"""
+test_input = """L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"""
 puzzle_input = str(open("Day1Input.txt").read())
 default_pos = 50
 current_pos = default_pos
-prev_pos = current_pos
 password = 0
 
 def ingester(string):
     
     instructions = []
     instructions = string.split("\n")
-    for i in range(len(instructions)):
+    for i in range(len(instructions)-1):
+        print(i, instructions[i])
         instructions[i] = [instructions[i][0],int(instructions[i][1:])]
     #we now have an array of instructions in the format ["L", 99]
     return instructions
     
 def dialer(current_pos, instruction):
-    z_clicks = 0
+    
     if instruction[0] == "L":           # simulate a turn to the left or right
         current_pos -= instruction[1]
     elif instruction[0] == "R":
@@ -35,23 +26,18 @@ def dialer(current_pos, instruction):
         
     while current_pos < 0:                 # correct for looping set of numbers
         current_pos += 100
-        z_clicks += 1
     while current_pos > 99:
         current_pos -= 100
-        z_clicks += 1
-    if current_pos == 0 and z_clicks == 0:
-        z_clicks += 1
-    return current_pos, z_clicks
+    return current_pos
 
 #-------------------------------------------------
-instructions = ingester(test_input)
-#instructions = ingester(puzzle_input)
+#instructions = ingester(test_input)
+instructions = ingester(puzzle_input)
 
-for i in range(len(instructions)):
-    dial_result = dialer(current_pos, instructions[i])
-    current_pos = dial_result[0]
-    print(instructions[i], prev_pos, current_pos, dial_result[1])
-    password += dial_result[1]
-    prev_pos = current_pos
+for i in range(len(instructions)-1):
+    current_pos = dialer(current_pos, instructions[i])
+    print(current_pos)
+    if current_pos == 0:
+        password += 1
         
 print("\nPassword: ", password)
